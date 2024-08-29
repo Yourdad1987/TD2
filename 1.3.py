9d346d1d4d1a819d4c68d604fdd32489f4314871
Yourdad1987 <108910208+Yourdad1987@users.noreply.github.com>
2024-08-29 11:29:58 -0400

    Create 1.3.py

---
+++ 1.3.py
@@ -0,0 +1,11 @@
+#>>> r, pi = 12 , 3.14159
+#on assigne la valeur 12 en int à la variable r et on assigne la valeur 3.14159 en float à la variable pi
+
+#>>> s = pi * r**2
+#on assigne la valeur de la variable pi multiplié à la valeur de la variable r au carré à la variable s
+
+#>>> print(s)
+#on fait afficher la valeur de la variable s au terminal
+
+#>>> print(type(r), type (pi), type(s))
+#on fait afficher le type de la valeur r, le type de la valeur pi et le type de la valeur s au terminal
