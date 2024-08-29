08050be2c503a00880cfa989f90e9980f7701435
Yourdad1987 <108910208+Yourdad1987@users.noreply.github.com>
2024-08-29 11:15:51 -0400

    doc +1.2

---
+++ 1.2.py
@@ -0,0 +1,9 @@
+#>>> largeur = 20 
+#on assigne la valeur 20 en int à la variable largeur
+
+#>>> hauteur = 5 * 9.3
+#on assigne la valeur de 5 multiplié par 9.3 en float à la variable hauteur
+
+#>>> largeur * hauteur
+#on multiplie la valeur de largeur par la valeur de hauteur
+
